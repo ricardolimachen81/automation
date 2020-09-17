208b118b06f112cbb2f35a87f5e7f4031af32781
Ricardo Lima Chen <ricardo.chen@accenture.com>
2020-09-17 14:15:55 -0300

    commit

--- automacao/src/test/java/com/cucumber/runner/Runner.java
+++ automacao/src/test/java/com/cucumber/runner/Runner.java
@@ -16,7 +16,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 	features = { "src/test/resources/features" }, 
-	tags = { "@RealizarBuscaInexistente" }, 
+	tags = { "@all" }, 
 	glue = {"com.cucumber.stepdefinitions", "com.cucumber.runner" }, 
 	plugin = {"pretty", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html",
 	"json:output/Cucumber.json"}, monochrome = true)
